095dbcc51ef7f1f81e4e14c6533031926fddd179
billygun27 <billygun27@gmail.com>
2019-04-24 10:06:35 +0700

    add bluetooth

--- movetest.py
+++ movetest.py
@@ -4,7 +4,8 @@ import robot.movement as move
 
 if __name__ == '__main__':
 	bot = MeAuriga()
-	bot.start("/dev/ttyUSB0")
+	#bot.start("/dev/ttyUSB0")
+	bot.startWithSerial("/dev/rfcomm0")
 	while 1:
 		sleep(0.1)
 		move.MoveForward(bot)
--- ultrasonicSensorRead.py
+++ ultrasonicSensorRead.py
@@ -5,7 +5,8 @@ def onRead(v):
 
 if __name__ == '__main__':
 	bot = MeAuriga()
-	bot.start("/dev/ttyUSB0")
+	#bot.start("/dev/ttyUSB0")
+	bot.startWithSerial("/dev/rfcomm0")
 	while 1:
 		sleep(0.1)
 		bot.ultrasonicSensorRead(9,onRead)
