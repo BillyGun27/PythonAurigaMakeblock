578c1ea2520890befbfd31226e1ee0fb351b5cfb
billygun27 <billygun27@gmail.com>
2019-04-25 11:53:40 +0700

    n

--- encoderMotorRun.py
+++ encoderMotorRun.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
 	bot = MegaPi()
 
 	bot.start()
-	bot.encoderMotorRun(4,0);
-	sleep(1);
+	bot.encoderMotorRun(4,0)
+	sleep(1)
 	while 1:
-		bot.encoderMotorRun(4,-200);
-		sleep(5);
-		bot.encoderMotorRun(4,0);
-		sleep(5);
+		bot.encoderMotorRun(4,-200)
+		sleep(5)
+		bot.encoderMotorRun(4,0)
+		sleep(5)
